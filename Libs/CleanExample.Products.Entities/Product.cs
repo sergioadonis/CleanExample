3e55c4b19306c05f87b59a6a70183b71f2b6d41b
Sergio Adonis <sergio.adonis@hotmail.com>
2020-01-31 15:58:16 -0600

    Refactoring src folder

---
+++ Libs/CleanExample.Products.Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using CleanExample.Common.Entities;
+
+namespace CleanExample.Products.Entities
+{
+    public class Product : AbstractEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
